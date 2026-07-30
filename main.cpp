bff4d9e9418c33d358dcf58c650b5b865f80293b
Noah Forkenbrock <asr.computing.main@gmail.com>
2026-07-30 16:09:43 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "Interpreter.h"
 
-int main(){ 
-    VirtualMachine VM; 
-    VM.RunProgram("Demos/17_full_demo.asr"); //Change this path for the file you'd like to run!
+int main(int argc, char* argv[]) {
+    if(argc > 1){
+        std::string name = argv[1]; 
+        VirtualMachine VM; 
+        VM.RunProgram(name);
+    }else{
+        VirtualMachine VM; 
+        VM.RunProgram("Demos/17_full_demo.asr");
+    }
     return 0; 
 }
-
