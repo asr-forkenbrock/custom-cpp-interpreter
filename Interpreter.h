1de585a3969dfa68dc7d87dcf04c99e30c9ecf5e
Noah Forkenbrock <asr.computing.main@gmail.com>
2026-07-31 21:21:41 -0400

    Fixed AST memory leak - added header

--- Interpreter.h
+++ Interpreter.h
@@ -1,3 +1,22 @@
+/*
+Interpreter.h
+
+ASR Interpreter - Lexer, Parser, and Interpreter written in C++.
+Copyright (C) 2026  Noah Forkenbrock <asr-forkenbrock> 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
@@ -99,6 +118,9 @@ class VirtualMachine {
         void ExecuteFunctionCall(ActionNode* input);
         void ExecuteStatement(ActionNode* input);
         void ExecuteProgramAST();
+
+        void RecurseRemoveAST(ActionNode* root, int &depth, int param); 
+        void RemoveAST(); 
 
     public: 
         void RunProgram(std::string name);
