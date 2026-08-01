b2bedcbd343d413584e9c053f164db38e121c7c2
Noah Forkenbrock <asr.computing.main@gmail.com>
2026-07-31 21:25:12 -0400

    added header

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,6 +1,24 @@
-#include "Lexer.h"
+/*
+Lexer.cpp
 
-        
+ASR Interpreter - Lexer, Parser, and Interpreter written in C++.
+Copyright (C) 2026  Noah Forkenbrock <asr-forkenbrock> 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Lexer.h"
 
         void LEXER::LoadProgram(std::string filename){
             std::cout << "Loading Program file...\n";
